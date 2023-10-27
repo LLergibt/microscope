cf929b885a2e28ad90380b7aece652d88fb00326
LLergibt <gosha2007naz@gmail.com>
2023-10-27 16:50:24 +0300

    Перенес функционал потока микроскопа в роут предназначенный для этого

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,7 @@
 from typing import Union
-import asyncio
 from users.route_users import router as user_router
 from microscopes.routes_microscope import router as microscope_router
 from database_connect import create_db_and_tables
-from pydantic import BaseModel
-from aiortc import RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaPlayer, MediaRelay
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -29,12 +25,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-relay = None
-webcam = None
-
-class Offer(BaseModel):
-    sdp: str
-    type: str
 
 @app.get("/")
 def read_root():
@@ -47,56 +37,3 @@ def on_startup():
     create_db_and_tables()
 
 
-def create_local_tracks(play_from=None):
-    global relay, webcam
-
-    if play_from:
-        player = MediaPlayer(play_from)
-        return player.audio, player.video
-    else:
-        options = {"framerate": "30", "video_size": "640x480"}
-        if relay is None:
-            webcam = MediaPlayer("/dev/video0", format="v4l2", options=options)
-            relay = MediaRelay()
-        return None, relay.subscribe(webcam.video)
-
-@app.post("/offer")
-async def offer(params: Offer):
-    offer = RTCSessionDescription(sdp=params.sdp, type=params.type)
-
-    pc = RTCPeerConnection()
-    pcs.add(pc)
-
-    @pc.on("connectionstatechange")
-    async def on_connectionstatechange():
-        print("Connection state is %s" % pc.connectionState)
-        if pc.connectionState == "failed":
-            await pc.close()
-            pcs.discard(pc)
-
-    # open media source
-    audio, video = create_local_tracks()
-
-    await pc.setRemoteDescription(offer)
-    for t in pc.getTransceivers():
-        if t.kind == "audio" and audio:
-            pc.addTrack(audio)
-        elif t.kind == "video" and video:
-            pc.addTrack(video)
-
-    answer = await pc.createAnswer()
-    await pc.setLocalDescription(answer)
-
-    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
-
-
-pcs = set()
-args = ''
-
-
-@app.on_event("shutdown")
-async def on_shutdown():
-    # close peer connections
-    coros = [pc.close() for pc in pcs]
-    await asyncio.gather(*coros)
-    pcs.clear()
