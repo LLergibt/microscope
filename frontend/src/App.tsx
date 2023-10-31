918d8d089413e9cce1144f786b8aff6c712d1f32
LLergibt <gosha2007naz@gmail.com>
2023-10-31 03:34:47 +0300

    style: Убрал логи из app, форматировал

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,24 @@
-import { createSignal } from 'solid-js'
-import Translater from './components/Translater'
-import Login from './components/Login'
-import Navbar from './components/Layout/Navbar'
-import UserProvider from './contexts/UserProvider'
+import Translater from "./components/Translater";
+import Login from "./components/Login";
+import Navbar from "./components/Layout/Navbar";
+import UserProvider from "./contexts/UserProvider";
 import { Router, Route, Routes } from "@solidjs/router";
-import { A } from "@solidjs/router"; 
-
 
 function App() {
-  const [count, setCount] = createSignal(0)
-
   return (
     <>
-   <Router>
-    <UserProvider>
-      <Routes>
-      <Route path="/" component={Navbar}>
-            <Route path="" component={Translater}/>
-            <Route path="/login" component={Login}/>
-      </Route>
-      </Routes>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" component={Navbar}>
+              <Route path="" component={Translater} />
+              <Route path="/login" component={Login} />
+            </Route>
+          </Routes>
+        </Router>
       </UserProvider>
-   </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
