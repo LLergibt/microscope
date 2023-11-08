6dc6a8e59dd778455d3950d3a283cb573cad8e29
LLergibt <gosha2007naz@gmail.com>
2023-11-08 06:29:41 +0300

    fix: Типизировал хук

--- frontend/src/hooks/useClickOutside.ts
+++ frontend/src/hooks/useClickOutside.ts
@@ -1,14 +1,11 @@
-import { createEffect } from 'solid-js';
-//import {RefreshContext} from 'components/layout'
-//
 import { onCleanup } from "solid-js";
+import type { JSX } from "solid-js";
 
-export function useClickOutside(el, accessor) {
-  const onClick = (e) => {
-    !el().contains(e.target) && accessor();}
+export function useClickOutside(el: JSX.Element, accessor: () => void) {
+  const onClick = (e: Event) => {
+    !el().contains(e.target) && accessor();
+  };
   document.body.addEventListener("click", onClick);
 
   onCleanup(() => document.body.removeEventListener("click", onClick));
 }
-
-
