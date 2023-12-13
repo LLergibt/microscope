cc2a12b948db4cc5bc5c7b97d554dcb5b26827b7
LLergibt <gosha2007naz@gmail.com>
2023-12-13 07:59:13 +0300

    refactor: Добавил ключи для firebase в репозиторий

--- frontend/src/services/firebase.ts
+++ frontend/src/services/firebase.ts
@@ -9,13 +9,13 @@ import {
 
 import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBW5dRcsggB-GteyAhjY8TXotc4uRGlbPA",
+  authDomain: "microscope-49942.firebaseapp.com",
+  projectId: "microscope-49942",
+  storageBucket: "microscope-49942.appspot.com",
+  messagingSenderId: "821597752838",
+  appId: "1:821597752838:web:ba96a0a37f7a4cbc54fd87",
+  measurementId: "G-3XF36V5BJJ",
 };
 
 export const app = initializeApp(firebaseConfig);
